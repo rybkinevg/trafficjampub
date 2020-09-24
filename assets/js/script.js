4b6d39f056a83fe458430a83e775a1e309126196
rybkinevg <boomuk-mc@yandex.ru>
2020-09-24 16:55:01 +0300

    almost done html/css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
+
     $('.news-carousel').slick({
         arrows: false,
         slidesToShow: 4,
         dots: true
     });
+
+    $('.menu-carousel').slick({
+        arrows: false,
+        slidesToShow: 3,
+        dots: true
+    });
+
+    $('.review-carousel').slick({
+        arrows: false,
+        slidesToShow: 1,
+        dots: true
+    });
+
 })
