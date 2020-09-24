1a6d235f6f44d1a67b67903ada6984852fe0e8f3
rybkinevg <boomuk-mc@yandex.ru>
2020-09-24 18:24:01 +0300

    almost

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,15 @@ $(document).ready(function () {
         dots: true
     });
 
+    ymaps.ready(init);
+
+    function init() {
+        myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+    }
+
 })
